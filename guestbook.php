7978544b1c296d39b28c1b2500b19380a8720f5c
Sebastian Hase <sebastian_hase@gmx.de>
2015-01-09 16:48:54 +0100

    FIx

--- guestbook.php
+++ guestbook.php
@@ -43,7 +43,7 @@ if ($pmp_guestbook_showcode == true) {
 	$smarty->assign('imgLoc', $imgLoc);
 }
 
-if (isset($_GET['action']) && ($_GET['action'] == 'save') {
+if (isset($_GET['action']) && $_GET['action'] == 'save') {
 	// First check the form key
 	if (!isset($_POST['form_key']) || !$formKey->validate()) {
 		//Form key is invalid, show an error
